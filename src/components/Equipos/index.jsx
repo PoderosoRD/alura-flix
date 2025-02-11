2850955693bcfa6a6ce7a911f67d9aae7678f534
Orlando <paulinorpk@gmail.com>
2025-02-11 16:14:00 -0400

    Modificacion de la seccion equipos

--- src/components/Equipos/index.jsx
+++ src/components/Equipos/index.jsx
@@ -1,6 +1,8 @@
 
 import ConexionAPI from "../../../ConexionAPI";
 import "./Equipos.css"
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { AiOutlineEdit } from "react-icons/ai";
 
 const Equipos = () => {
     return (
@@ -9,19 +11,131 @@ const Equipos = () => {
             <button className="boton__front-end">FRONT END</button>
             <ul className="lista__videos">
                 <li>
-                    <iframe className="contenedor__videos" width="100%" height="100%" 
-                    src="https://www.youtube.com/embed/QjOWz9avkg8">
-                    </iframe>
+                    <figure className="contenedor__videos">
+                        <img className="imagen"  src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line /></span>
+                                <p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure>
                 </li> 
                 <li>
-                    <iframe className="contenedor__videos" width="100%" height="100%"  
-                    src="https://www.youtube.com/embed/QjOWz9avkg8">
-                    </iframe>
+                    <figure className="contenedor__videos"> 
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line />
+                                </span><p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure> 
                 </li> 
                 <li>
-                    <iframe className="contenedor__videos" width="100%" height="100%"
-                    src="https://www.youtube.com/embed/QjOWz9avkg8">
-                    </iframe>
+                    <figure className="contenedor__videos">
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos"><span className="icono"><RiDeleteBin2Line /></span><p>Borarr</p></div>
+                            <div className="editar__videos__elementos"><span className="icono"><AiOutlineEdit /></span><p>Editar</p></div>
+                        </div>
+                    </figure>
+                </li> 
+            </ul>
+            </article>
+            <article>
+            <button className="boton__front-end">FRONT END</button>
+            <ul className="lista__videos">
+                <li>
+                    <figure className="contenedor__videos">
+                        <img className="imagen"  src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line /></span>
+                                <p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure>
+                </li> 
+                <li>
+                    <figure className="contenedor__videos"> 
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line />
+                                </span><p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure> 
+                </li> 
+                <li>
+                    <figure className="contenedor__videos">
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos"><span className="icono"><RiDeleteBin2Line /></span><p>Borarr</p></div>
+                            <div className="editar__videos__elementos"><span className="icono"><AiOutlineEdit /></span><p>Editar</p></div>
+                        </div>
+                    </figure>
+                </li> 
+            </ul>
+            </article>
+            <article>
+            <button className="boton__front-end">FRONT END</button>
+            <ul className="lista__videos">
+                <li>
+                    <figure className="contenedor__videos">
+                        <img className="imagen"  src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line /></span>
+                                <p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure>
+                </li> 
+                <li>
+                    <figure className="contenedor__videos"> 
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><RiDeleteBin2Line />
+                                </span><p>Borarr</p>
+                            </div>
+                            <div className="editar__videos__elementos">
+                                <span className="icono"><AiOutlineEdit /></span>
+                                <p>Editar</p>
+                            </div>
+                        </div>
+                    </figure> 
+                </li> 
+                <li>
+                    <figure className="contenedor__videos">
+                        <img className="imagen" src="https://www.youtube.com/embed/QjOWz9avkg8"/>
+                        <div className="editar__video">
+                            <div className="editar__videos__elementos"><span className="icono"><RiDeleteBin2Line /></span><p>Borarr</p></div>
+                            <div className="editar__videos__elementos"><span className="icono"><AiOutlineEdit /></span><p>Editar</p></div>
+                        </div>
+                    </figure>
                 </li> 
             </ul>
             </article>
